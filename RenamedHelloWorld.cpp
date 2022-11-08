1df34711bb4198ec27e08434be5d9ab1119d71fc
Kilian Horn <kilian.horn@inform-software.com>
2022-11-08 10:54:43 +0100

    Hello there! General Kenobi!

---
+++ RenamedHelloWorld.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+/**
+ * @brief This File contains a Function to print "Hello World!"
+ *
+ * @return int Successcode
+ */
+int main()
+{
+    std::cout << "Hello World";
+    return 0;
+}
